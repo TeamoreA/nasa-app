abdbb6907a7ad0ce9f779602eb140d5bac1a7bd6
Timothy Kamau <tim@Timothys-MacBook-Pro.local>
2020-11-09 11:59:45 +0300

    Add dashboard view - add a dasboard to display items - add a sidebar for filtering asteroids

---
+++ src/components/SideDrawer.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card flat height="400px">
+    <v-list dense>
+      <v-list-item>
+        <v-list-item-content>
+          <v-menu
+            v-model="start_menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="start_date"
+                label="Start date"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="start_date"
+              @input="end_menu = false"
+            ></v-date-picker>
+          </v-menu>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="end_date"
+                label="End date"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="end_date"
+              @input="menu2 = false"
+            ></v-date-picker>
+          </v-menu>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "SideDrawer",
+  data() {
+    return {
+      start_date: new Date().toISOString().substr(0, 10),
+      end_date: new Date().toISOString().substr(0, 10),
+      // menu: false,
+      // modal: false,
+      start_menu: false,
+      end_menu: false
+    };
+  }
+};
+</script>
