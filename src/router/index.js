3209ac168c3dbee6aa97e6bb4acdfe84064c2c53
Timothy Kamau <tim@Timothys-MacBook-Pro.local>
2020-11-09 12:01:28 +0300

    Add single asteroid view - add view to display a single asteroid

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,34 @@ const routes = [
     path: "/",
     name: "Dashboard",
     component: Dashboard
+  },
+  {
+    path: "/asteroid/:id",
+    name: "Asteroid",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Asteroid.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Register.vue")
   }
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
 ];
 
 const router = new VueRouter({
